4931a00e31e265712914160a1df31a109844247b
Djuan Gayle <77689134+DjuanG98@users.noreply.github.com>
2023-03-14 15:10:47 -0500

    Update firebase.js
    Removed Firebase Key & other personal details

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/firestore'
 
 // firebase init - add your own config here
 const firebaseConfig = {
-  apiKey: "AIzaSyCrr57Wqs0ytsdw2MhiI6wx4wp8vViPH6w",
-  authDomain: "personal-site-7b6b8.firebaseapp.com",
-  databaseURL: "https://personal-site-7b6b8.firebaseio.com",
-  projectId: "personal-site-7b6b8",
-  storageBucket: "personal-site-7b6b8.appspot.com",
-  messagingSenderId: "885999530094",
-  appId: "1:885999530094:web:29b0b89092c3cc7611767f",
-  measurementId: "G-BFQV49CB2J"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 }
 firebase.initializeApp(firebaseConfig)
 firebase.analytics();
